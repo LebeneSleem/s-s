cf20e739a63398bcc1db001287cb440d30f980a5
NanaAba08 <williamsnanaaba@gmail.com>
2023-06-26 03:57:32 -0700

    Handle the PATH

--- shell.h
+++ shell.h
@@ -13,10 +13,17 @@
 
 #define BUFFER_SIZE 1024
 #define Max_Command 20
+#define PATH_MAX 1024
 
 void swap_int(int *a, int *b);
 void bubble_sort(int *array, size_t size);
 void prompt_command(char **av, char **env);
+int string_len(char *string);
+char *string_cpy(char *dest, char *src);
+int string_cmp(char *s1, char *s2);
+char *_strdup(char *str);
+void _env(char **env);
+char* handle_path(char* command);
 
 
 #endif
